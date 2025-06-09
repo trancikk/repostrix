3b720ab572264462a1f247ef3d15c73789740688
pwnzk <25871766+trancikk@users.noreply.github.com>
2025-06-08 22:32:35 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+import * as dotenv from 'dotenv';
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { mkdirSync, createWriteStream } from "node:fs";
+import { Writable } from "node:stream";
+
+dotenv.config();
+// ensure photos directory exists
+mkdirSync("./photos", { recursive: true });
+
+if (process.env.BOT_TOKEN === undefined) {
+    throw new TypeError("BOT_TOKEN must be provided!");
+}
+
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+
+});
+
+// small helper
+const download = async (fromFileId: string, toPath: string) => {
+    const link = await bot.telegram.getFileLink(fromFileId);
+    const res = await fetch(link.toString());
+    await res.body!.pipeTo(Writable.toWeb(createWriteStream(toPath)));
+};
+
+bot.on('channel_post', async ctx => {
+    console.log(ctx.senderChat)
+})
+// handler that downloads all photos the bot sees to a photos
+bot.on(message("photo"), async ctx => {
+    // take the last photosize (highest size)
+    const { file_id } = ctx.message.photo.pop()!;
+    // const path = `./photos/${file_id}.jpg`;
+    const link = await bot.telegram.getFileLink(file_id);
+    // await download(file_id, path);
+    // console.log("Downloaded", path);
+    await bot.telegram.sendPhoto('-1002326830163', file_id)
+});
+
+bot.launch();
