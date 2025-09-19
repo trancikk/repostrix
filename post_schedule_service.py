0439b585bcaabe42f9b04ea26b991bf3670eb236
pwnzk <25871766+trancikk@users.noreply.github.com>
2025-09-18 23:25:28 -0700

    updated logging

--- post_schedule_service.py
+++ post_schedule_service.py
@@ -21,19 +21,18 @@ async def send_post(session: AsyncSession, bot_wrapper: BotWrapper, post: Post):
     # TODO might be time delay due to sending latency, to think if that might be an issue
     now = get_now()
     post_is_sent = False
-    print(post.source_chat.next_fire_time)
-    print(post.source_chat.last_posted_at)
+    logging.info("About to send post.id: %s", post.id)
     if post.is_album:
         media_group_bd = MediaGroupBuilder(caption=post.text)
         for asset in post.assets:
             # TODO handle different types of content video\photo\etc
             media_group_bd.add(type='photo', media=asset.file_id)
-        # TODO make it parallel
 
         for chat in post.target_chats:
             next_fire_dt = nvl(now, chat.next_fire_time, post.source_chat.next_fire_time)
             if next_fire_dt > now:
                 continue
+            logging.info("Sending album post.id %s to channel.id %s, fire time was %s", post.id, chat.id, next_fire_dt)
             await bot_wrapper.send_media_group(chat_id=chat.id, media=media_group_bd.build())
             chat.last_posted_at = now
             post_is_sent = True
@@ -42,6 +41,7 @@ async def send_post(session: AsyncSession, bot_wrapper: BotWrapper, post: Post):
             next_fire_dt = nvl(now, chat.next_fire_time, post.source_chat.next_fire_time)
             if next_fire_dt > now:
                 continue
+            logging.info("Sending post %s to channel %s, fire time was %s", post.id, chat.id, next_fire_dt)
             await bot_wrapper.copy_message(post.source_message_id, post.source_chat_id,
                                            chat.id)
             chat.last_posted_at = get_now()
@@ -49,12 +49,11 @@ async def send_post(session: AsyncSession, bot_wrapper: BotWrapper, post: Post):
     # TODO post should be 'posted' if it was posted at least to some channels (currently its updated even if its not posted)
     # TODO fixed with a flag kludge for now
     if post_is_sent:
+        logging.info("Sent post.id %s, updating statuses...", post.id)
         post.status = PostStatus.POSTED
         post.posted_at = now
         post.source_chat.last_posted_at = now
     await session.commit()
-    print(post.source_chat.next_fire_time)
-    print(post.source_chat.last_posted_at)
 
 
 async def run_schedule_service(bot_wrapper: BotWrapper):
