1d491ba22c8b0dd3f04be8ed37883e74e78eee93
pwnzk <25871766+trancikk@users.noreply.github.com>
2025-09-20 16:05:31 -0700

    added post cancel functionality

--- bot/routes/post.py
+++ bot/routes/post.py
@@ -5,11 +5,14 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.models import ChatStatus
-from db.repo import create_post_from_message, update_post_schedule, find_channel_by_id
+from db.models import ChatStatus, PostStatus
+from db.repo import create_post_from_message, update_post_schedule, find_channel_by_id, find_post
 from utils import get_not_empty_string
 
+CANCEL_REPOSTING = 'cancel'
+
 time_table = {
+    "Cancel reposting": CANCEL_REPOSTING,
     "Next 1 mins": 1 / 60,
     "Next 5 mins": 1 / 12,
     "Next 10 mins": 1 / 6,
@@ -45,23 +48,29 @@ async def save_message(message: Message, session: AsyncSession, album: list[Mess
                                                       source_chat_id=message.chat.id, text=text, files=photos,
                                                       is_album=len(photos) > 0)
         logging.info(f"Created new post: {created_post} from {message.chat.title} ({message.chat.id})")
-        await message.answer("Post saved. What time do you want to post it? Default is next hour",
-                             reply_markup=get_time_table_kb(created_post.id))
+        await message.reply("Post saved. What time do you want to post it? Default is next hour",
+                            reply_markup=get_time_table_kb(created_post.id))
 
 
 @post_router.callback_query(F.data.startswith('schedule_post'))
-async def handle_schedule_update(callback_data: CallbackQuery, session: AsyncSession) -> None:
+async def handle_schedule_update(callback_data: CallbackQuery, session: AsyncSession):
     try:
         data = callback_data.data.split(':')[-1]
+        await callback_data.answer('Processing...')
         if data is not None:
             post_id, delta = data.split('-')
             if post_id is not None and delta is not None:
+                if delta == CANCEL_REPOSTING:
+                    post = await find_post(session, int(post_id))
+                    if post is not None:
+                        post.status = PostStatus.CANCELLED
+                        return await callback_data.message.answer('Cancelled this post',
+                                                                  reply_to_message_id=post.source_message_id)
                 updated_post = await update_post_schedule(session, int(post_id), float(delta))
-                await session.commit()
                 if updated_post is not None:
-                    # await schedule_message(bot_wrapper, updated_post.id)
                     logging.info(f"Scheduled post {post_id} to {updated_post.scheduled_at}")
-                    await callback_data.message.answer(f"Scheduled post will be posted in the next {delta} hours")
+                    return await callback_data.message.answer(
+                        f"Scheduled post will be posted in the next {delta} hours")
     except Exception as e:
         logging.error(f"Error during schedule callback handling: {e}")
         logging.exception(e)
